737e693aac786d57b650a8d796acc1c172da8412
kairat.tilegen <kairat.tilegen@latoken.com>
2025-09-29 18:32:13 +0500

    add constants

---
+++ app/constants.py
@@ -0,0 +1,22 @@
+from envparse import env
+
+REDIS_URL = ''
+
+GOOGLE_SERVICE_ACCOUNT_FILE = env.str("GOOGLE_SERVICE_ACCOUNT_FILE",
+                                      default="")
+amo_crm_link = ''
+amocrm_subdomain = ''
+
+GOOGLE_SHEET_ID = ''
+GOOGLE_WORKSHEET_NAME = ""
+
+CREATE_PIPELINE_ID =
+CREATE_STATUS_ID =
+RESPONSIBLE_USER_ID =
+
+LEAD_COMMENT_ID =
+LEAD_EMAIL_ID =
+LEAD_PHONE_ID =
+LEAD_BUDGET_ID = 0
+
+ACCESS_TOKEN = ""
